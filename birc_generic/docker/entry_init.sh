5f69d621e0389e66033a6885e66c032238ddb468
rhancockn <rhancock@gmail.com>
2017-11-17 17:43:06 -0500

    Various updates and CUDA

--- birc_generic/docker/entry_init.sh
+++ birc_generic/docker/entry_init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #entrypoint pre-initialization
+source /environment
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+#matlab
+export PATH=/bind/bin/matlab/bin:"$PATH"
+export LD_LIBRARY_PATH=/bind/bin/matlab/bin/glnxa64:${LD_LIBRARY_PATH}
+export LM_LICENSE_FILE=/bind/licenses/matlab.lic
+#cuda
+export PATH=/bind/lib/cuda/bin:"$PATH"
+export LD_LIBRARY_PATH=/bind/lib/cuda/lib64:${LD_LIBRARY_PATH}
 
 #TODO: configure this for ANTs
 #export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS
