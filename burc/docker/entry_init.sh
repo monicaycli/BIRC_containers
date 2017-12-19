5837292e1bd6dbb17a4ba500b365e0b7d549e661
rhancockn <rhancock@gmail.com>
2017-12-19 10:48:09 -0500

    Customize command prompt. Fixes #8

--- burc/docker/entry_init.sh
+++ burc/docker/entry_init.sh
@@ -6,7 +6,8 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 #matlab
 export PATH=/bind/bin/matlab/bin:"$PATH"
 export LD_LIBRARY_PATH=/bind/bin/matlab/bin/glnxa64:${LD_LIBRARY_PATH}
-
+export PS1="\u@\h(burc):\W\\$ "
+export prompt="[%n@%m(burc):%c]%# "
 #cuda
 #export PATH=/bind/lib/cuda/bin:"$PATH"
 #export LD_LIBRARY_PATH=/bind/lib/cuda/lib64:${LD_LIBRARY_PATH}
