82bb92be4720da395c1a2dda8d98b6c64da74a9a
Roeland Hancock <rhancock@gmail.com>
2017-12-24 17:53:11 -0500

    Templates for xanadu

---
+++ example_xanadu/burc_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Run the container
+#Delete any unused bind points below
+#if you have permissions, you can add :ro to the end of bind options
+# to restrict writing in the container
+singularity run  --bind $(dirname `which matlab`)/..:/bind/bin/matlab \
+--bind ${DIR_DATA}:/bind/data \
+--bind ${DIR_DATAIN}:/bind/data_in \
+--bind ${DIR_DATAOUT}:/bind/data_out \
+--bind ${SUBJECTS_DIR}:/bind/freesurfer \
+--bind ${DIR_RESOURCES}:/bind/resources \
+--bind ${DIR_SCRATCH}:/bind/scratch \
+--bind ${DIR_WORK}:/bind/work \
+--bind ${DIR_SCRIPTS}:/bind/scripts \
+/home/CAM/${USER}/containers/bin/burc.img "$@"
+
+#add this line to limit access to home and /tmp
+#--contain --workdir ${DIR_TMP} \
---
+++ example_xanadu/mk_skel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Make a directory skeleton for BIRC containers
+PROJECT=$1
+mkdir -p /home/CAM/${USER}/${PROJECT}/{data,data_in,data_out,freesurfer,scratch,scripts,resources,work}
+
